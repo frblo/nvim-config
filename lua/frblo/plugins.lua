78bc44f09c9949d74c5333872bb6382ba4048333
Fredrik Blomqvist <frblo@kth.se>
2023-02-28 15:58:40 +0100

    Add treesitter

--- lua/frblo/plugins.lua
+++ lua/frblo/plugins.lua
@@ -72,6 +72,14 @@ return packer.startup(function(use)
 	use "nvim-telescope/telescope-fzf-native.nvim" -- Telescope fzf native
 	use 'nvim-telescope/telescope-media-files.nvim' -- Telescope media files
 
+	-- Treesitter
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+  	}
+	use "p00f/nvim-ts-rainbow"
+	use "nvim-treesitter/playground"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
